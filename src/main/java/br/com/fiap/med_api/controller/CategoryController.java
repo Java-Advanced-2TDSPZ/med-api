bc10a93c770dfb2ba4699767adf60edef4cadee7
Gabrielly Macedo <162237578+gabimaced0@users.noreply.github.com>
2025-02-25 21:11:55 -0300

    removendo imports

--- src/main/java/br/com/fiap/med_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/med_api/controller/CategoryController.java
@@ -3,14 +3,11 @@ package br.com.fiap.med_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 import br.com.fiap.med_api.model.Category;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //component
