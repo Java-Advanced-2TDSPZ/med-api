cde2f9cc1dfb50c327c7ff1a1dadf8f062267da6
Gabrielly Macedo <162237578+gabimaced0@users.noreply.github.com>
2025-02-25 21:07:28 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/med_api/model/Category.java
@@ -0,0 +1,36 @@
+package br.com.fiap.med_api.model;
+
+import java.util.Random;
+
+public class Category {
+    private Long id;
+    private String name;
+    private String icon;
+
+
+    public Category(Long id, String name, String icon) {
+        this.id = (id == null) ? new Random().nextLong() : id;
+        this.name = name;
+        this.icon = icon;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getIcon() {
+        return icon;
+    }
+    
+    
+
+
+
+}
---
+++ src/test/java/br/com/fiap/med_api/MedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.med_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
